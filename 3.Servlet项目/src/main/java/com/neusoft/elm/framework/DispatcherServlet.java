700bb8467b5195e549798c89a4971324e8818bea
liangyiming <363939725@qq.com>
2022-09-05 14:13:39 +0800

    jackson prepared

--- 3.Servlet项目/src/main/java/com/neusoft/elm/framework/DispatcherServlet.java
+++ 3.Servlet项目/src/main/java/com/neusoft/elm/framework/DispatcherServlet.java
@@ -42,8 +42,8 @@ public class DispatcherServlet extends HttpServlet {
             Object result = method.invoke(controller, new Object[]{request});
             //获取向客户端响应的输出流
             out = response.getWriter();
-            ObjectMapper om = new ObjectMapper();
             //向客户端响应json数据
+            ObjectMapper om = new ObjectMapper();
             out.print(om.writeValueAsString(result));
         } catch (Exception e) {
             e.printStackTrace();
