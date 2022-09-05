d8b8cae73b0fde7d9854923539836a54deea1492
liangyiming <363939725@qq.com>
2022-09-05 23:55:37 +0800

    Servlet seventh commit

--- 3.Servlet项目/后端部分/src/main/java/com/neusoft/elm/po/OrderDetailet.java
+++ 3.Servlet项目/后端部分/src/main/java/com/neusoft/elm/po/OrderDetailet.java
@@ -8,4 +8,44 @@ public class OrderDetailet {
 
     //多对一：所属食品
     private Food food;
+
+    public Integer getOdId() {
+        return odId;
+    }
+
+    public void setOdId(Integer odId) {
+        this.odId = odId;
+    }
+
+    public Integer getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(Integer orderId) {
+        this.orderId = orderId;
+    }
+
+    public Integer getFoodId() {
+        return foodId;
+    }
+
+    public void setFoodId(Integer foodId) {
+        this.foodId = foodId;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public Food getFood() {
+        return food;
+    }
+
+    public void setFood(Food food) {
+        this.food = food;
+    }
 }
