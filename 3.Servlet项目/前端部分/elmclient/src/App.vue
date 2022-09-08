bba2c933a0ce192626e60a7eb13142451f8a47ce
briller <291912103@qq.com>
2022-09-08 17:44:06 +0800

    文件整理

---
+++ 3.Servlet项目/前端部分/elmclient/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <router-view />
+  </div>
+</template>
+
+<style>
+html,
+body,
+div,
+span,
+h1,
+h2,
+h3,
+h4,
+h5,
+h6,
+ul,
+ol,
+li,
+p {
+  margin: 0;
+  padding: 0;
+}
+html,
+body,
+#app {
+  width: 100%;
+  height: 100%;
+  font-family: "微软雅黑";
+}
+ul,
+ol {
+  list-style: none;
+}
+a {
+  text-decoration: none;
+}
+</style>
