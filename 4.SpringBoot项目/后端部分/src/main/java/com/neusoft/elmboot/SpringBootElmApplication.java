13f68c66cd09101ee1a0a2bf25d0deb75b5abc8f
briller <291912103@qq.com>
2022-09-13 16:28:01 +0800

    SpringBoot项目部署

---
+++ 4.SpringBoot项目/后端部分/src/main/java/com/neusoft/elmboot/SpringBootElmApplication.java
@@ -0,0 +1,13 @@
+package com.neusoft.elmboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootElmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootElmApplication.class, args);
+    }
+
+}
